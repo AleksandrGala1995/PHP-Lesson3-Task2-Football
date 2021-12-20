2293b0e410b3b57c2b2e8f1e31c4564675701b13
AleksandrGala1995 <95771059+AleksandrGala1995@users.noreply.github.com>
2021-12-20 20:27:07 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo "Роки у промужку з 2000 по 2100, у які проводиться ЧС з футболу: ";
+$year=2002;
+do {
+   echo "$year "; 
+   $year=$year+4;
+    
+}
+while ($year<=2100);
+
+?>
